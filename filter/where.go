1e4ef4e6d4578a4962ba08734250bdf39a922237
Muflih Kholidin <muflic.24@gmail.com>
2023-01-23 08:11:35 +0700

    Revise cast int8 til int64 and uint8 til uint64

--- filter/where.go
+++ filter/where.go
@@ -36,6 +36,22 @@ func (w Where) getValue(val interface{}) string {
 	switch v.Type().Name() {
 	case "int":
 		return strconv.Itoa(val.(int))
+	case "uint8":
+		return strconv.Itoa(int(val.(uint8)))
+	case "uint16":
+		return strconv.Itoa(int(val.(uint16)))
+	case "uint32":
+		return strconv.Itoa(int(val.(uint32)))
+	case "uint64":
+		return strconv.Itoa(int(val.(uint64)))
+	case "int8":
+		return strconv.Itoa(int(val.(int8)))
+	case "int16":
+		return strconv.Itoa(int(val.(int16)))
+	case "int32":
+		return strconv.Itoa(int(val.(int32)))
+	case "int64":
+		return strconv.Itoa(int(val.(int64)))
 	case "float64":
 		return fmt.Sprintf("%f", val)
 	case "float32":
